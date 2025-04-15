f6a378254eb53bb74e0d1fdc395f7b30b9eefbe7
zed07 <zed43281@gmail.com>
2025-04-15 00:05:11 -0400

    Display error messages on submission

--- blog/blog.py
+++ blog/blog.py
@@ -1,8 +1,9 @@
 from sqlite3 import Error, IntegrityError
 
-from flask import (Blueprint, flash, g, redirect, render_template, request, url_for, json)
+from flask import (Blueprint, flash, g, redirect, render_template, request, url_for, json, abort)
 
 from blog.db import get_db
+
 
 bp = Blueprint('blog', __name__, url_prefix='/blog')
 
@@ -10,21 +11,26 @@ bp = Blueprint('blog', __name__, url_prefix='/blog')
 
 @bp.route('/create', methods=('GET','POST'))
 def createBlogPost():
-    if request.method == 'POST':
-        try:
-            data = request.get_json()
-            title = data['title']
-            blog_summary = data['blogSummary']
-            blog_content = data['blogContent']
+    if request.method == 'POST': 
+        data = request.get_json()
+        title = data['title']
+        blog_summary = data['blogSummary']
+        blog_content = data['blogContent']
+        error = None
 
-            if not title:
-                return json.jsonify({"status": 400, "message": "A title must be included"})
-            if not blog_summary:
-                return json.jsonify({"status": 400, "message": "A blog summary must be included"})
-            if not blog_content:
-                return json.jsonify({"status": 400, "message": "Blog content must be included"})
-            return json.jsonify({"status": 200, "message": "Blog post created successfully"})
-        except Exception as e: 
-            print("An unexpected error occured: ", e)
+        """
+        If there are input errors, log them 
+        in order to render them
+        """
+        if not title:
+            error = 'A title must be included.'
+        elif not blog_summary:
+            error = 'A blog summary must be included.'
+        elif not blog_content:
+            error = 'Blog content must be included.'
 
+        if error is None:
+            print("success!")
+        else:
+            return json.jsonify({"error": error}), 400
     return render_template('blog/blog_form.html')
